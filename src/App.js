88ea92a3058bebf3520af215c858a0fce7db286f
SimoneCheng <bleach1827@gmail.com>
2021-11-26 13:51:16 +0800

    feature: fix react-router-dom bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Index from './components/Index';
@@ -18,22 +18,20 @@ function App() {
   return (
     <>
       <Header />
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Index} />
-          <Route path="/templates" exact component={Templates} />
-          <Route path="/templates/:id" component={MemeGenerator} />
-          <Route path="/personal" exact component={Personal} />
-          <Route path="/personal/meme-generator/:id" component={MemeGenerator} />
-          <Route path="/setting" component={Setting} />
-          <Route path="/meme/:id" component={Meme} />
-          <Route path="/public/:id" exact component={Public} />
-          <Route path="/explorememes" component={AllMemes} />
-          <Route path="/uploadtemplate" component={UploadTemplate} />
-          <Route path="/search" component={AllMemes} />
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" exact component={Index} />
+        <Route path="/templates" exact component={Templates} />
+        <Route path="/templates/:id" component={MemeGenerator} />
+        <Route path="/personal" exact component={Personal} />
+        <Route path="/personal/meme-generator/:id" component={MemeGenerator} />
+        <Route path="/setting" component={Setting} />
+        <Route path="/meme/:id" component={Meme} />
+        <Route path="/public/:id" exact component={Public} />
+        <Route path="/explorememes" component={AllMemes} />
+        <Route path="/uploadtemplate" component={UploadTemplate} />
+        <Route path="/search" component={AllMemes} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
     </>
   );
 }
