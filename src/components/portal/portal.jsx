c861ac867b03974974ad95a6ca3ae83457e6aefb
Simone <58110624+SimoneCheng@users.noreply.github.com>
2023-11-26 21:07:13 +0800

    feat: add portal component (#10)
    * feat: add portal component
    * fix: fix typo
    * refactor: use arrow function

---
+++ src/components/portal/portal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+
+const Portal = ({
+  children,
+  containerRef: containerRefProp
+}) => {
+  const containerRef = containerRefProp ?? document.body;
+
+  if (containerRef) {
+    return createPortal((
+      <div data-portal="">
+        {children}
+      </div>
+    ), containerRef);
+  }
+
+  return null;
+}
+
+export default Portal;
