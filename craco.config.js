0de121fe58857c6f9f7663fcb48e7b26dcc56e67
Simone <58110624+SimoneCheng@users.noreply.github.com>
2024-04-11 22:19:31 +0800

    chore: add webpack-bundle-analyzer (#35)

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    plugins: [
+      new BundleAnalyzerPlugin({ analyzerMode: "server" }),
+    ]
   },
 };
