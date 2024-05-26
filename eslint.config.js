a965a715fc37b7dcee64c92ca9a9837e21cbcc33
SimoneCheng <bleach1827@gmail.com>
2024-05-26 21:09:03 +0800

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+const settings = [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ...pluginReactConfig.languageOptions,
+    }
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ['src/**/*.{js,jsx}'],
+    ...pluginReactConfig,
+  },
+  {
+    rules: {
+      semi: ['error', 'always'],
+      "no-unused-vars": "error",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    }
+  }
+];
+
+export default settings;
